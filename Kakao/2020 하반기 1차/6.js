70abe2fce93fd03352e4d052dcf8e9de7ec8a95d
신지민 <kahin6105@gmail.com>
2022-01-26 23:07:04 +0900

    카카오 외벽점검 성공

--- Kakao/2020 하반기 1차/6.js
+++ Kakao/2020 하반기 1차/6.js
@@ -1,37 +1,39 @@
 function solution(n, weak, dist) {
   const numFriends = dist.length
+  const longWeak = Array(weak.length * 2)
   const picked = Array(numFriends).fill(0)
-  const checked = Array(weak.length).fill(0)
   const MAX = 1000
   let ans = MAX
 
+  for (let i = 0; i < weak.length; i++) {
+    longWeak[i] = weak[i]
+    longWeak[i + weak.length] = weak[i] + n
+  }
+
   const getNextWeakIdx = (curWeakIdx, friendIdx) => {
     let canGo = dist[friendIdx]
-    let before = weak[curWeakIdx]
+    let before = longWeak[curWeakIdx]
     let i = curWeakIdx
 
     while (canGo >= 0) {
       let diff
-      i = (i + 1) % weak.length
+      i++
 
-      if (before > weak[i]) {
-        const tmp = weak[i] + n
-        diff = tmp - before
-      } else {
-        diff = weak[i] - before
+      if (i >= longWeak.length) {
+        return i
       }
 
+      diff = longWeak[i] - before
+
       canGo -= diff
-      before = weak[i]
+      before = longWeak[i]
     }
 
     return i
   }
 
-  const cntFriends = (curIdx) => {
-    if (checked[curIdx]) {
-      console.log('picked', picked)
-      console.log('checked', checked)
+  const cntFriends = (startIdx, curIdx) => {
+    if (curIdx - startIdx >= weak.length) {
       return picked.reduce((prev, cur) => prev + cur)
     }
 
@@ -41,37 +43,19 @@ function solution(n, weak, dist) {
       if (picked[friend]) {
         continue
       }
-      console.log('friend', friend)
-      const nextIdx = getNextWeakIdx(curIdx, friend)
-      let checkCnt
 
-      if (curIdx < nextIdx) {
-        checkCnt = nextIdx - curIdx
-      } else {
-        checkCnt = (nextIdx + weak.length - curIdx) % weak.length
-      }
+      const nextIdx = getNextWeakIdx(curIdx, friend)
 
       picked[friend] = 1
-      for (let i = 0; i < checkCnt; i++) {
-        const idx = (i + curIdx + weak.length) % weak.length
-        checked[idx] = 1
-      }
-
-      minFriends = Math.min(minFriends, cntFriends(nextIdx))
-
+      minFriends = Math.min(minFriends, cntFriends(startIdx, nextIdx))
       picked[friend] = 0
-      for (let i = 0; i < checkCnt; i++) {
-        const idx = (i + curIdx + weak.length) % weak.length
-        checked[idx] = 0
-      }
     }
 
     return minFriends
   }
 
   for (let startWeak = 0; startWeak < weak.length; startWeak++) {
-    console.log('weak[startWeak]', weak[startWeak])
-    ans = Math.min(ans, cntFriends(startWeak))
+    ans = Math.min(ans, cntFriends(startWeak, startWeak))
   }
 
   if (ans === MAX) {
